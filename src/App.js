0f9aa73ff60d2e9c52c5a069754bee47e6531a3a
divyansh pal <70535666+divyansh343@users.noreply.github.com>
2022-07-25 18:43:08 +0530

    initialized prozect using cra

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div >
+       <p className="text-purple-900 text-lg bottom-4 border-black bg-neutral-900">
+        Welcom to zumanzi
+        </p> 
+    </div>
     </div>
   );
 }
