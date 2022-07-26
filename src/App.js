29a9bd62ef7015a2147a1fa4d0e043a9fd7153c3
divyansh pal <70535666+divyansh343@users.noreply.github.com>
2022-07-26 08:49:48 +0530

    export to csv is working

--- src/App.js
+++ src/App.js
@@ -38,6 +38,33 @@ function App() {
     clearValues()
   }
 
+  const handleExportToCsv = () => {
+    var content = store;
+
+    var finalVal = '';
+
+    for (var i = 0; i < content.length; i++) {
+      var value = content[i];
+
+      for (var j = 0; j < value.length; j++) {
+        var innerValue = value[j];
+        var result = innerValue.replace(/"/g, '""');
+        if (result.search(/("|,|\n)/g) >= 0)
+          result = '"' + result + '"';
+        if (j > 0)
+          finalVal += ',';
+        finalVal += result;
+      }
+
+      finalVal += '\n';
+    }
+
+    var pom = document.createElement('a');
+    pom.setAttribute('href', 'data:text/csv;charset=utf-8,' + encodeURIComponent(finalVal));
+    pom.setAttribute('download', 'test.csv');
+    pom.click();
+  }
+
   const previewData = (e) => {
     e.preventDefault()
 
@@ -141,6 +168,7 @@ function App() {
 
             <button onClick={previewData} className="my-2 mx-2 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">preview</button>
 
+            <button onClick={handleExportToCsv} className="my-2 mx-2 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Export to csv</button>
 
           </form>
 
